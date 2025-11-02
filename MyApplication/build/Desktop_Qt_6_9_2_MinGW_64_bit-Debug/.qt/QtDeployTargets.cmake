16a1943f8291587f62fa1cd366c1203d81b32ab3
Yarik <yarik77.97@mail.ru>
2025-11-02 22:54:14 +0300

    Убрал папку build

--- MyApplication/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyApplication_FILE R:/informationsafe/lab1/MyApplication/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/MyApplication.exe)
-set(__QT_DEPLOY_TARGET_MyApplication_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MyApplication_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
